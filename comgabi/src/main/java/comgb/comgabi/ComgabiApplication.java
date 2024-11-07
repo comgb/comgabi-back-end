29b258ddda2a0d5678bb984daa8b5717dab930c7
이예준 <pipining4470@naver.com>
2024-11-07 19:27:59 +0900

    INITIAL

---
+++ comgabi/src/main/java/comgb/comgabi/ComgabiApplication.java
@@ -0,0 +1,13 @@
+package comgb.comgabi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComgabiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ComgabiApplication.class, args);
+	}
+
+}
---
+++ comgabi/src/test/java/comgb/comgabi/ComgabiApplicationTests.java
@@ -0,0 +1,13 @@
+package comgb.comgabi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ComgabiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
