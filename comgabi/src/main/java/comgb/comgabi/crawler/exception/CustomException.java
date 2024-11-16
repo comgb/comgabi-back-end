535ef04950240a954a03a26f86fb1e3840af29b7
이예준 <pipining4470@naver.com>
2024-11-17 01:42:12 +0900

    DELETE

--- comgabi/src/main/java/comgb/comgabi/crawler/exception/CustomException.java
+++
@@ -1,7 +0,0 @@
-package comgb.comgabi.crawler.exception;
-
-public class CustomException extends RuntimeException {
-    public CustomException(String message) {
-        super(message);
-    }
-}
--- comgabi/src/main/java/comgb/comgabi/crawler/repository/CrawledDataRepository.java
+++
@@ -1,9 +0,0 @@
-package comgb.comgabi.crawler.repository;
-
-import comgb.comgabi.crawler.model.CrawledData;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface CrawledDataRepository extends JpaRepository<CrawledData, Long> {
-}
